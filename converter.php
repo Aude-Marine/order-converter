ff88e7ca345aafdaf8c49563318cf43171e72135
Aude-Marine <adanede17@jcu.edu>
2021-10-10 19:36:18 +0200

    Load the XML files

---
+++ converter.php
@@ -0,0 +1,8 @@
+<?php
+
+$xmlOrders = simplexml_load_file("Orders.xml") or die("Error: Cannot create object");
+
+$xmlExchangeRates = simplexml_load_file("ExchangeRates.xml") or die("Error: Cannot create object");
+
+
+?>
