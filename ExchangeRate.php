4264f32265373cc352fc58919577db17777e90d3
Aude-Marine <adanede17@jcu.edu>
2021-10-10 20:18:27 +0200

    Loading Currency EXchange Rates

--- ExchangeRate.php
+++ ExchangeRate.php
@@ -17,6 +17,31 @@ class ExchangeRate {
     public function setCurrencyArray($currencyArray) {
         $this->currencyArray = $currencyArray;
     }
+
+    public function loadCurrency($xmlExchangeRates) {
+        foreach($xmlExchangeRates->currency as $currency){
+            $currencyArray = null;
+            foreach($currency->rateHistory->rates as $rates) {
+                $currencyRateValue = "";
+                foreach ($rates as $rate) {
+                    foreach($rate->attributes() as $a => $b) {
+                        if($b!="EUR" && $b!="GBP" && $b!="1") {
+                            $currencyRateValue = $b;
+                        }
+                    }
+                    $currencyArray = [
+                        'date' => $rates['date'],
+                        'rate' =>$currencyRateValue,
+                    ];
+                }
+                if($currency->code=="GBP") {
+                    $this->addCurrencyArray($currencyArray);
+                } else {
+                    $this->addCurrencyArray($currencyArray);
+                }
+            }
+        }    
+    }
 }
 
 ?>
--- converter.php
+++ converter.php
@@ -1,9 +1,12 @@
 <?php
     require_once('ExchangeRate.php');
-    require_once('Order.php');
+    require_once('Orders.php');
 
     $xmlOrders = simplexml_load_file("Orders.xml") or die("Error: Cannot create object");
 
     $xmlExchangeRates = simplexml_load_file("ExchangeRates.xml") or die("Error: Cannot create object");
+
+    $order = new Order();
+    $order->setXmlOrders($xmlOrders);
 
 ?>
